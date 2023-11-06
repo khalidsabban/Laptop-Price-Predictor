89ed433cbc646d30fcbd3c2c3a56321b891d2710
Khalid sabban <77420857+khalidsbn@users.noreply.github.com>
2023-11-06 17:27:50 +0100

    Update model.py

--- app/model.py
+++ app/model.py
@@ -1,5 +1,7 @@
-import pandas as pd
+""" ML model file"""
+
 import pickle
+import pandas as pd
 from functions import ram_extractor, feature_encoder
 from xgboost import XGBRegressor
 
@@ -14,7 +16,12 @@ df = feature_encoder(df, encoding_cols)
 X, y = df.drop('Price_euros', axis=1), df['Price_euros']
 
 # Train model
-xgb_params = {'max_depth': 8, 'eta':0.1, 'subsample': 0.8, 'colsample_bytree': 0.6, 'seed': 2018, 'objective': 'reg:squarederror'}
+xgb_params = {'max_depth': 8, 
+              'eta':0.1, 
+              'subsample': 0.8, 
+              'colsample_bytree': 0.6, 
+              'seed': 2018, 
+              'objective': 'reg:squarederror'}
 xgb = XGBRegressor(**xgb_params)
 xgb.fit(X, y)
 
