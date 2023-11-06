74a1d5b75d39df6fe0b968ff2d39e3eabafb2050
Khalid sabban <77420857+khalidsbn@users.noreply.github.com>
2023-11-06 17:27:32 +0100

    Update functions.py

--- app/functions.py
+++ app/functions.py
@@ -1,11 +1,20 @@
+""" functions to use them in app.py and model.py to process data """
+
 from sklearn.preprocessing import OrdinalEncoder
 
 # Processing
-def ram_extractor(X):
-    X['Ram'] = X['Ram'].str.replace('GB', '').astype(int)
-    return X
+def ram_extractor(df):
+    """
+    Extract ram feature
+    return ram feature as integer (From 4GB to 4 after using ram_extractor)
+    """
+    df['Ram'] = df['Ram'].str.replace('GB', '').astype(int)
+    return df
 
-def feature_encoder(X, cols):
+def feature_encoder(df, cols):
+    """
+    Processing object features with OrdinalEncoder
+    """
     ordinal_encoder = OrdinalEncoder()
-    X[cols] = ordinal_encoder.fit_transform(X[cols])
-    return X
+    df[cols] = ordinal_encoder.fit_transform(df[cols])
+    return df
